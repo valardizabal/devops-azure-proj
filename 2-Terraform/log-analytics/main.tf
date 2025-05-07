bdc72f419f5ae989908ee7c7a8a42ce5070a9948
valardizabal <va.lardizabal@gmail.com>
2025-05-07 07:44:36 +0000

    [Add] Added files for terraform log anaytics

---
+++ 2-Terraform/log-analytics/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_version = ">= 1.9.6"
+  backend "azurerm" {
+    resource_group_name  = "devops-proj-rg"
+    storage_account_name = "devopsprojst"
+    container_name       = "tfstate"
+    key                  = "la-terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "8c6f346b-200d-4475-99b4-d26874174cbd"
+
+}
+
+data "azurerm_resource_group" "resource_group" {
+  name = var.rgname
+}
+
+resource "azurerm_log_analytics_workspace" "Log_Analytics_WorkSpace" {
+  # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
+  name                = "${var.name}-la"
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+  sku                 = "PerGB2018"
+
+  tags = var.tags
+
+}
+
+resource "azurerm_log_analytics_solution" "Log_Analytics_Solution_ContainerInsights" {
+  solution_name         = "ContainerInsights"
+  location              = azurerm_log_analytics_workspace.Log_Analytics_WorkSpace.location
+  resource_group_name   = data.azurerm_resource_group.resource_group.name
+  workspace_resource_id = azurerm_log_analytics_workspace.Log_Analytics_WorkSpace.id
+  workspace_name        = azurerm_log_analytics_workspace.Log_Analytics_WorkSpace.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
+}
---
+++ 2-Terraform/log-analytics/terraform.tfvars
@@ -0,0 +1,9 @@
+name     = "devopsproj-vl"
+location = "southeastasia"
+rgname   = "devops-proj-rg"
+
+tags = {
+  "DeployedBy"  = "Terraform"
+  "Environment" = "production"
+  "Projects"    = "DevOps"
+}
